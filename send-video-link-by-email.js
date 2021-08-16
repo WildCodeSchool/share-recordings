0d0f82afbd7c556ae276edf8f218bfafc0addddd
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-08-16 16:55:03 +0200

    send link in mail

--- send-video-link-by-email.js
+++ send-video-link-by-email.js
@@ -1,12 +1,17 @@
 const emailer = require('./emailer');
 
+const link = process.argv[2];
+
 emailer.sendMail(
   {
     from: 'noreply@wilder.school',
     to: 'jitsi-trainers@wildcodeschool.com',
     subject: 'New conference recording is available',
-    text: 'Hello world',
-    html: '<p>Hello <em>world</em></p>',
+    text: 'The recording is available here : ' + link,
+    html:
+      '<p>The recording is available <a href="' +
+      link +
+      '" _target="blank">here</a></p>',
   },
   (err, info) => {
     if (err) console.error(err);
